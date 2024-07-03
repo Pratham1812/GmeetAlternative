98b79c3c1f2798b22faa99dd335a419109780917
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-04 01:29:49 +0530

    rfac: modifies signalingServer to add the funtionality to a class

--- backend/src/services/signalingServer.ts
+++ backend/src/services/signalingServer.ts
@@ -1,50 +1,53 @@
-import express from 'express';
 import http from 'http';
 import {Server,Socket} from 'socket.io';
 
-const app=express();
-const server=http.createServer(app);
-const io=new Server(server);
+class SignalingServer{
+    private io: Server;
 
-io.on('connection',(socket:Socket)=>{
-    console.log("A user connected");
+    constructor(server: http.Server){
+        this.io =new Server(server);
+        this.initializeSocketEvents();
+    }
 
-    socket.on('join',(roomName)=>{
-        const {rooms}=io.sockets.adapter;
-        const room=rooms.get(roomName);
+    private initializeSocketEvents(){
+        this.io.on('connection',(socket:Socket)=>{
+            console.log("A user connected");
+        
+            socket.on('join',(roomName)=>{
+                const {rooms}=this.io.sockets.adapter;
+                const room=rooms.get(roomName);
+        
+                if(room==undefined){
+                    socket.join(roomName);
+                    socket.emit('created');
+                }else{
+                    socket.join(roomName);
+                    socket.emit('joined');
+                }
+            })
+        
+            socket.on('ready',(roomName)=>{
+                socket.broadcast.to(roomName).emit("ready");
+            })
+        
+            socket.on('offer',(offer,roomName)=>{
+                socket.broadcast.to(roomName).emit("offer",offer);
+            })
+        
+            socket.on('answer',(answer,roomName)=>{
+                socket.broadcast.to(roomName).emit("answer",answer);
+            })
+        
+            socket.on('leave',(roomName)=>{
+                socket.leave(roomName);
+                socket.broadcast.to(roomName).emit("leave");
+            })
+        
+            socket.on('disconnect',()=>{
+                console.log('A user disconnected');
+            })
+        })
+    }
+}
 
-        if(room==undefined){
-            socket.join(roomName);
-            socket.emit('created');
-        }else{
-            socket.join(roomName);
-            socket.emit('joined');
-        }
-    })
-
-    socket.on('ready',(roomName)=>{
-        socket.broadcast.to(roomName).emit("ready");
-    })
-
-    socket.on('offer',(offer,roomName)=>{
-        socket.broadcast.to(roomName).emit("offer",offer);
-    })
-
-    socket.on('answer',(answer,roomName)=>{
-        socket.broadcast.to(roomName).emit("answer",answer);
-    })
-
-    socket.on('leave',(roomName)=>{
-        socket.leave(roomName);
-        socket.broadcast.to(roomName).emit("leave");
-    })
-
-    socket.on('disconnect',()=>{
-        console.log('A user disconnected');
-    })
-})
-
-server.listen(3001,()=>{
-    console.log('socket.io server running on port 3001');
-})
-
+export default SignalingServer;
