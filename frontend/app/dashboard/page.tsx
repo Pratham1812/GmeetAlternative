6eafba7e02e5747ef672fa57cbc803f3a792a5c1
Mohit Verma <mohit_v@ch.iitr.ac.in>
2024-07-04 09:48:29 +0530

    dashboard and room design fixed package.json

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,9 +1,28 @@
+"use client"
 import Navbar from '@/components/Navbar'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const page = () => {
+  const router = useRouter()
+  const [roomid, setRoomid] = useState('')
+
+  const joinRoom = () => {
+    router.push(`/room/${roomid || Math.random().toString(36).slice(2)}`)
+  }
+
   return (
+    <>
     <Navbar/>
+    <div className="">
+
+      <main className="flex flex-col gap-4 justify-center w-screen items-center ">
+       <h1>Lets join a room!</h1>
+       <input onChange={(e) => setRoomid(e.target.value)} value={roomid} className="w-96 border rounded-md" />
+       <button onClick={joinRoom} type="button" className="p-3 text-black rounded-md bg-slate-400">Join Room</button>
+      </main>
+    </div>
+    </>
   )
 }
 
