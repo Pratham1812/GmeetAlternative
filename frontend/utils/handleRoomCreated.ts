2b68568c86ae9d9681851aa6d3916fb7da550d20
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-05 10:56:55 +0530

    feat:adds handleRoomCreated function

--- frontend/utils/handleRoomCreated.ts
+++ frontend/utils/handleRoomCreated.ts
@@ -1,5 +1,27 @@
 
-const handleRoomCreated=()=>{
-
+const handleRoomCreated=(
+    hostRef:React.MutableRefObject<boolean>,
+    userStreamRef:React.MutableRefObject<MediaStream|null>,
+    userVideoRef:React.RefObject<HTMLVideoElement|null>
+)=>{
+    hostRef.current=true;
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video:{
+            width:500,height:500
+        },
+    }).then((stream)=>{
+        userStreamRef.current=stream;
+        if(userVideoRef.current!=null){
+            userVideoRef.current.srcObject=stream;
+            userVideoRef.current.onloadedmetadata=()=>{
+                userVideoRef.current?.play();
+            }
+        }else{
+            console.log("Failed to fetch video stream");
+        }
+    }).catch((error)=>{
+        console.log(error);
+    })
 }
 export default handleRoomCreated;
