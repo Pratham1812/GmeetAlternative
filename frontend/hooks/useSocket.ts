50d0a9bd42e3e76119360935e2919c47fdefb398
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-05 14:14:47 +0530

    feat: modifies useSocket.ts

--- frontend/hooks/useSocket.ts
+++ frontend/hooks/useSocket.ts
@@ -1,25 +1,41 @@
-import { useEffect,useRef } from "react";
+import { useEffect,useRef, useState } from "react";
 import {io,Socket} from 'socket.io-client';
 
 const useSocket=(url:string):Socket|null=>{
     const socketCreated=useRef(false);
-    const socket=useRef<Socket|null>(null);
+    const [socket,setSocket]=useState<Socket|null>(null);
 
     useEffect(()=>{
         if(!socketCreated.current){
             const socketInitializer =async()=>{
-                socket.current=io(url);
+                const socketInstance=  io("http://localhost:4000");
+                socketInstance.connect();
+                console.log(socketInstance);
+                socketInstance.on('connect',()=>{
+                    setSocket(socketInstance);
+                    console.log(socketInstance);
+                    console.log("socket connected");
+                });
+
+                socketInstance.on("connect_error",(error)=>{
+                    console.log("connection error",error);
+                })
+                
+                socketInstance.on("disconnect",(error)=>{
+                    console.log("socket disconnected");
+                    setSocket(null);
+                })
             }
             try{
                 socketInitializer();
                 socketCreated.current=true;
             }catch(error){
-                console.log(error);
+                console.log(error,"failed");
             }
         }
     },[url])
 
-    return socket.current;
+    return socket;
 }
 
 export default useSocket;
