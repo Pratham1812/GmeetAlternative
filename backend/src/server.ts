7ddb443e5177b31d6cdcd7eaba6ec8758b1e8661
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-09 02:06:53 +0530

    feat: adds transcribe post request endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,6 +30,28 @@ app.get('/',(req,res) => {
     res.send("Hello World")
 })
 
+app.post('/transcribe',async(req,res)=>{
+  const data=req.body;
+
+  try{
+    const response =await fetch(
+      "https://api-inference.huggingface.co/models/openai/whisper-small",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.SPEECH_TO_TEXT_WHISPER_SMALL_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: data,
+      }
+    );
+    const result = await response.json();
+    return result;
+  }catch(error:any){
+    res.status(500).json({error:error.message});
+  }
+});
+
 app.post('/summarize', async (req, res) => {
     const { transcript } = req.body;
   
