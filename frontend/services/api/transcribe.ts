4d9f5e4a0eb291c18b2a1207dff8b8347e736065
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-09 21:05:22 +0530

    feat: modifies transcribe.ts function to return text response from api call and adds console.log statements for debugging

--- frontend/services/api/transcribe.ts
+++ frontend/services/api/transcribe.ts
@@ -3,20 +3,20 @@ import axios, { AxiosResponse } from "axios";
 const transcribe=async(
     audioBlob:React.MutableRefObject<Blob|null>
 ):Promise<string|undefined>=>{
+    console.log("inside transcribe api call",audioBlob.current);
     try{
         const formData=new FormData();
         if(audioBlob.current){
-            formData.append('file',audioBlob.current,'recording.wav');
-            const response= await axios.post("http://localhost:4000/transcribe",{
-                body:formData
-            },
-        {
-            headers:{
-                "Content-Type":"application/json",
-            },
-        }
-    )
-    return response.data;
+            formData.append('file',audioBlob.current,'recording.flac');
+            console.log(formData);
+            const response= await axios.post("http://localhost:4000/transcribe",formData,
+            {
+                headers:{
+                    "Content-Type":"multipart/form-data",
+                },
+            });
+            console.log(response.data.text);
+            return response.data.text;
         }else{
             console.log("failed to fetch audio file");
         }
