b8d3ef1b0383ae1161c62814cbc7c28fb0ff7864
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-09 08:55:22 +0530

    feat: modifies Room component to summarize audio to text

--- frontend/app/room/[id]/page.tsx
+++ frontend/app/room/[id]/page.tsx
@@ -17,6 +17,7 @@ import toggleMic from '@/utils/toggleMic';
 import toggleCamera from '@/utils/toggleCamera';
 import handleScreenShare from '@/utils/handleScreenShare';
 import stopAudioRecording from '@/utils/stopAudioRecording';
+import endMeeting from '@/utils/endMeeting';
 
 
 const Room = () => {
@@ -28,7 +29,7 @@ const Room = () => {
   const param = useSearchParams();
   const mediaRecorder=useRef<MediaRecorder|null>(null);
   const audioChunks=useRef<Blob[]|null>(null);
-  const audioUrl=useRef<String|null>(null);
+  const audioBlob=useRef<Blob|null>(null);
   const userVideoRef = useRef<HTMLVideoElement | null>(null);
   const peerVideoRef = useRef<HTMLVideoElement | null>(null);
   const screenShareRef = useRef<HTMLVideoElement>(null);
@@ -37,13 +38,13 @@ const Room = () => {
   const hostRef = useRef<boolean>(false);
   const roomNameParam:string|null=param.get('roomName');
   const roomName:string=roomNameParam?roomNameParam:"";
-  const [summary, setSummary] = useState('');
+  const summary=useRef<string|null>(null);
 
   useEffect(() => {
     if (socket != null) {
       socket.emit('join', roomName);
-      socket.on("created", () => handleRoomCreated(hostRef,userStreamRef, userVideoRef,mediaRecorder,audioChunks,audioUrl));
-      socket.on("joined", () => handleRoomJoined(userStreamRef, userVideoRef, socket, roomName,mediaRecorder,audioChunks,audioUrl));
+      socket.on("created", () => handleRoomCreated(hostRef,userStreamRef, userVideoRef,mediaRecorder,audioChunks,audioBlob));
+      socket.on("joined", () => handleRoomJoined(userStreamRef, userVideoRef, socket, roomName,mediaRecorder,audioChunks,audioBlob));
       socket.on("ready", () => initiateCall(hostRef, rtcConnectionRef, userStreamRef, socket, roomName, () =>
         createPeerConnection(
           (event) => handleICECandidateEvent(event, socket, roomName),
@@ -68,47 +69,7 @@ const Room = () => {
     }
   }, [roomName, socket]);
 
-  const endMeeting = async () => {
-    stopAudioRecording(mediaRecorder);
-    
-    const transcript = `New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.
-A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.
-Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.
-In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.
-Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the
-2010 marriage license application, according to court documents.
-Prosecutors said the marriages were part of an immigration scam.
-On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further.
-After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective
-Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002.
-All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say.
-Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages.
-Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted.
-The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s
-Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.
-Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force.
-If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18.`
-    try {
-      const response = await fetch('http://localhost:4000/summarize', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ transcript }),
-      });
-
-      const data = await response.json();
-      const summary_dict = data[0]
-      const summary = summary_dict.summary_text
-      console.log(summary)
-      setSummary(summary);
-    } catch (error) {
-      console.error('Error summarizing meeting:', error);
-    }
-
-    leaveRoom(roomName, userVideoRef, peerVideoRef, rtcConnectionRef, socket, router)
-  };
-
+  
   
 
   return (
@@ -116,7 +77,7 @@ If convicted, Barrientos faces up to four years in prison.  Her next court appea
       {summary ? <>
         <div className='flex flex-col p-10 '>
           <h3 className='font-bold text-3xl mb-4'>Meeting Summary:</h3>
-          <p className='text-lg'>{summary}</p>
+          <p className='text-lg'>{summary.current}</p>
           <button className=' mt-4 bg-red-400 text-black rounded-lg p-3 w-40' onClick={() => {router.push("/dashboard")}}>Go to Dashboard</button>
         </div>
       </> : <>
@@ -129,7 +90,7 @@ If convicted, Barrientos faces up to four years in prison.  Her next court appea
       <button className='mx-4  bg-red-400 text-black rounded-lg p-3' onClick={() => toggleMic(micActive, setMicActive, userStreamRef)} type="button">
         {micActive ? 'Mute Mic' : 'UnMute Mic'}
       </button>
-      <button className='mx-4  bg-red-400 text-black rounded-lg p-3' onClick={endMeeting} type="button">
+      <button className='mx-4  bg-red-400 text-black rounded-lg p-3' onClick={()=>endMeeting(mediaRecorder,audioBlob,roomName, userVideoRef, peerVideoRef, rtcConnectionRef, socket,summary)} type="button">
         Leave
       </button>
       <button className='mx-4 bg-red-400 text-black rounded-lg p-3' onClick={() => toggleCamera(cameraActive, setCameraActive, userStreamRef)} type="button">
