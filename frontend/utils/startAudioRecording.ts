138c2f0c9f4af67f611bb5d33b080b10ac1f18d1
Tanisha Dixit <dixittanisha02@gmail.com>
2024-07-09 21:10:22 +0530

    feat: modifies startAudioRecording.ts function to handle audio to call transcribe and summarize functions for audio to text-summary conversion and adds console.log statements for debugging

--- frontend/utils/startAudioRecording.ts
+++ frontend/utils/startAudioRecording.ts
@@ -1,26 +1,47 @@
+import summarize from "@/services/api/summarize";
+import transcribe from "@/services/api/transcribe";
 
 
 const startAudioRecording=(
     stream:MediaStream,
     mediaRecorder:React.MutableRefObject<MediaRecorder|null>,
     audioChunks:React.MutableRefObject<Blob[]|null>,
-    audioBlob:React.MutableRefObject<Blob|null>
+    audioBlob:React.MutableRefObject<Blob|null>,
+    summary:React.MutableRefObject<string|null>
+
 )=>{
     mediaRecorder.current=new MediaRecorder(stream);
+    console.log(mediaRecorder.current);
     mediaRecorder.current.ondataavailable=event=>{
+        console.log("fetching data from mediaRecorder");
         if(event.data.size>0){
-            audioChunks.current?.push(event.data);
+            console.log("adding data to audiochunks array");
+            if(audioChunks.current){
+                audioChunks.current.push(event.data);
+                console.log(audioChunks.current);
+            }else{
+                audioChunks.current=[];
+                audioChunks.current.push(event.data);
+                console.log(audioChunks.current);
+            }    
         }
     };
-
-    mediaRecorder.current.onstop=()=>{
+    mediaRecorder.current.onstop=async ()=>{
         if(audioChunks.current){
-            audioBlob.current=new Blob(audioChunks.current,{type:'audio/wav'});
+            audioBlob.current=new Blob(audioChunks.current,{type:'audio/flac'});
+            console.log(audioBlob.current);
+            const transcript = await transcribe(audioBlob);
+            console.log(transcript);
+            const result= await summarize(transcript?transcript:'');
+            if(result!=undefined){
+            summary.current=result;
+    }
         }else{
             console.log("Failed to fetch audioChunks array");
         }
     };
     mediaRecorder.current.start();
+    console.log(mediaRecorder.current);
 
 }
 export default startAudioRecording;
